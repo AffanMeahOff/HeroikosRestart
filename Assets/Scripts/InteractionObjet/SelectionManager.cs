b10be5223686cf767b71e46aba0e6dc9da654089
AffanMeahOff <affan.meah@epita.fr>
2025-01-12 19:18:03 +0100

    retour du selection manager

---
+++ Assets/Scripts/InteractionObjet/SelectionManager.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectionManager : MonoBehaviour
+{
+    public GameObject interaction_Info_UI;
+    Text interaction_text;
+    private InteractableObject currentInteractableObject;
+
+    private void Start()
+    {
+        interaction_text = interaction_Info_UI.GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var selectionTransform = hit.transform;
+            var interactableObject = selectionTransform.GetComponent<InteractableObject>();
+
+            if (selectionTransform.GetComponent<InteractableObject>())
+            {
+                interaction_text.text = interactableObject.GetItemName();
+                interaction_Info_UI.SetActive(true);
+                currentInteractableObject = interactableObject;
+
+                if (Input.GetKeyDown(KeyCode.E)) // Appuyez sur E pour ramasser
+                {
+                    PickUpObject(currentInteractableObject);
+                }
+            }
+            else
+            {
+                interaction_Info_UI.SetActive(false);
+                currentInteractableObject = null;
+            }
+        }
+        else
+        {
+            interaction_Info_UI.SetActive(false);
+            currentInteractableObject = null;
+        }
+    }
+
+    void PickUpObject(InteractableObject interactableObject)
+    {
+        if (interactableObject != null)
+        {
+            // Logique pour ramasser l'objet, par exemple l'ajouter à l'inventaire
+            Debug.Log("Ramassé : " + interactableObject.GetItemName());
+            // Vous pouvez soit désactiver l'objet
+            interactableObject.gameObject.SetActive(false);
+            // Ou le détruire
+            // Destroy(interactableObject.gameObject);
+        }
+    }
+} 
