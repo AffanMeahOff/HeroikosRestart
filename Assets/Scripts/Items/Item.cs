773524a84a2da82130f048380b6178c0a1f6554f
AffanMeahOff <affan.meah@epita.fr>
2025-01-15 09:12:29 +0100

    Les Scripts de Thiago push

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour
+{
+    public int Dégâts;
+    public int Durabilité;
+    public string ItemName;
+    private void Awake()
+    {
+        
+    }
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        
+    }
+}
