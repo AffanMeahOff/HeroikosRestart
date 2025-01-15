ea5f4dd091f836a5414707b2d65c713103500d67
Haddadene <lucie.haddadene@epita.fr>
2025-01-15 20:54:34 +0100

    interface monnaie

---
+++ Assets/Scripts/monnaie/Interface.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using TMPro;  // Pour TextMeshPro
+
+public class Interface : argent
+{
+    public TextMeshProUGUI DrachmesText;
+    
+    void Start()
+    {
+        base.Start();
+        UpdateUI();     // Mettre à jour l'UI dès le début
+    }
+
+    // mise à jour le texte de l'UI avec le montant actuel
+    void UpdateUI()
+    {
+        DrachmesText.text = "Drachmes : " + Montant.ToString();
+    }
+
+    // Gagner des Drachmes et mettre à jour l'UI
+    public void GagnerDrachmes(int valeur)
+    {
+        base.GagnerDrachmes(valeur);
+        UpdateUI();  // Mise à jour l'UI après un gain
+    }
+
+   
+    public void PayerDrachmes(int valeur)
+    {
+        if (base.PayerDrachmes(valeur))
+        {
+            UpdateUI();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
