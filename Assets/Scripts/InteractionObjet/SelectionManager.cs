835d45deb2c20889a099ebb51bebc0b626721a2a
Thiago Sicard <thiago.sicard@epita.fr>
2025-02-25 20:43:52 +0100

    Ajout du nouveau SelectionManager

--- Assets/Scripts/InteractionObjet/SelectionManager.cs
+++ Assets/Scripts/InteractionObjet/SelectionManager.cs
@@ -3,90 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+ 
 public class SelectionManager : MonoBehaviour
 {
+ 
     public GameObject interaction_Info_UI;
     Text interaction_text;
-
-    private InteractableObject currentInteractableObject;
-    public Camera Camera;
-    public GameObject NameObject;
-
-    
-
+ 
     private void Start()
     {
         interaction_text = interaction_Info_UI.GetComponent<Text>();
-    }   
-
+    }
+ 
     void Update()
     {
-        Ray ray = Camera.ScreenPointToRay(Input.mousePosition);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-
         if (Physics.Raycast(ray, out hit))
         {
             var selectionTransform = hit.transform;
-
-            if (selectionTransform != null)
+ 
+            if (selectionTransform.GetComponent<InteractableObject>())
             {
-                var interactableObject = selectionTransform.GetComponent<InteractableObject>();
-
-                if (interactableObject != null)
-                {
-                    interaction_Info_UI.SetActive(true);
-
-                    //interaction_text.text = interactableObject.GetItemName();
-                    currentInteractableObject = interactableObject;
-                    /*
-                    if (Input.GetKeyDown(KeyCode.P)) // Appuyez sur P pour ramasser
-                    {
-                        PickUpObject(currentInteractableObject);
-                    }
-                    */
-                }
-                else
-                {
-                    interaction_Info_UI.SetActive(false);
-                    currentInteractableObject = null;
-                }
+                interaction_text.text = selectionTransform.GetComponent<InteractableObject>().GetItemName();
+                interaction_Info_UI.SetActive(true);
             }
-        }
-        else
-        {
-            interaction_Info_UI.SetActive(false);
-            currentInteractableObject = null;
-        }
-
-        if(Input.GetKeyUp(KeyCode.P))
-        {
-            NameObject.SetActive(true);
+            else 
+            { 
+                interaction_Info_UI.SetActive(false);
+            }
+ 
         }
     }
+}
 
-    
-    /*
-    void PickUpObject(InteractableObject interactableObject)
-    {
-        if (interactableObject != null)
-        {
-            // Logique pour ramasser l'objet, par exemple l'ajouter à l'inventaire
-            string name = interactableObject.GetItemName();
-            Debug.Log("Picked Up : " + name);
-            // Vous pouvez soit désactiver l'objet
-
-            GameObject item = interactableObject.gameObject;
-            if (name == "Jar")
-            {
-                InventaireManager.Instance?.Add(item);
-            }
-
-
-            interactableObject.gameObject.SetActive(false);
-            // Ou le détruire
-            // Destroy(interactableObject.gameObject);
-        }
-    }
-    */
-} 
