624896311e1aa55e84e4782b4598806cf3a4340f
AffanMeahOff <affan.meah@epita.fr>
2025-05-21 11:56:45 +0200

    Fix Enigme1 bug, fix des tp, ajout de l'enigme 4 pour amine, reset des terrains pour reduire le lag

--- Assets/TpEnigme1.cs
+++ Assets/TpEnigme1.cs
@@ -13,19 +13,26 @@ public class TpEnigme1 : MonoBehaviour
             //gameObject.transform.SetPositionAndRotation(new Vector3(679, 560, 308), gameObject.transform.rotation);
             SceneManager.LoadScene("Enigme1"); 
         }
-        if(other.CompareTag("Teleporter2"))
+        if(other.CompareTag("TpEliott"))
         {
             gameObject.transform.position = new Vector3(679, 560, 308);
             Physics.SyncTransforms();
             //gameObject.transform.SetPositionAndRotation(new Vector3(679, 560, 308), gameObject.transform.rotation);
             SceneManager.LoadScene("Enigme2"); 
         }  
-        if(other.CompareTag("Teleporter3"))
+        if(other.CompareTag("TpThiago"))
         {
             gameObject.transform.position = new Vector3(679, 560, 308);
             Physics.SyncTransforms();
             //gameObject.transform.SetPositionAndRotation(new Vector3(679, 560, 308), gameObject.transform.rotation);
             SceneManager.LoadScene("Enigme3"); 
-        }       
+        }  
+        if(other.CompareTag("TpAmine"))
+        {
+            gameObject.transform.position = new Vector3(679, 560, 308);
+            Physics.SyncTransforms();
+            //gameObject.transform.SetPositionAndRotation(new Vector3(679, 560, 308), gameObject.transform.rotation);
+            SceneManager.LoadScene("Enigme4"); 
+        }         
     }
 }
