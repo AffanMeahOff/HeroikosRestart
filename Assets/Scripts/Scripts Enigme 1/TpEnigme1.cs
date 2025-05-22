511b3cee4c2cb9a3c916bb7391170cba0631897c
AffanMeahOff <affan.meah@epita.fr>
2025-05-22 20:24:02 +0200

    hauteur en plus pour l'enigme 2

--- Assets/Scripts/Scripts Enigme 1/TpEnigme1.cs
+++ Assets/Scripts/Scripts Enigme 1/TpEnigme1.cs
@@ -2,9 +2,11 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Unity.Netcode;
+using System.Runtime.CompilerServices;
 
 public class TpEnigme1 : MonoBehaviour
 {
+    [SerializeField] private PlayerMovement jumpscript;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Teleporter"))
@@ -14,26 +16,27 @@ public class TpEnigme1 : MonoBehaviour
             //gameObject.transform.SetPositionAndRotation(new Vector3(679, 560, 308), gameObject.transform.rotation);
             NetworkManager.Singleton.SceneManager.LoadScene("Enigme1", LoadSceneMode.Single);
         }
-        if(other.CompareTag("TpEliott"))
+        if (other.CompareTag("TpEliott"))
         {
             gameObject.transform.position = new Vector3(1084, 563, 84);
             Physics.SyncTransforms();
+            jumpscript.jumpHeight = 4;
             //gameObject.transform.SetPositionAndRotation(new Vector3(679, 560, 308), gameObject.transform.rotation);
             NetworkManager.Singleton.SceneManager.LoadScene("Enigme2", LoadSceneMode.Single);
-        }  
-        if(other.CompareTag("TpThiago"))
+        }
+        if (other.CompareTag("TpThiago"))
         {
             gameObject.transform.position = new Vector3(679, 560, 308);
             Physics.SyncTransforms();
             //gameObject.transform.SetPositionAndRotation(new Vector3(679, 560, 308), gameObject.transform.rotation);
             NetworkManager.Singleton.SceneManager.LoadScene("Enigme3", LoadSceneMode.Single);
-        }  
-        if(other.CompareTag("TpAmine"))
+        }
+        if (other.CompareTag("TpAmine"))
         {
             gameObject.transform.position = new Vector3(679, 560, 308);
             Physics.SyncTransforms();
             //gameObject.transform.SetPositionAndRotation(new Vector3(679, 560, 308), gameObject.transform.rotation);
             NetworkManager.Singleton.SceneManager.LoadScene("Enigme4", LoadSceneMode.Single);
-        }         
+        }
     }
 }
